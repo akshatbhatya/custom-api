5d5a699dbaecd306ed37b449180a94f288747861
akshat <akshatbhatya@gmail.com>
2024-03-29 20:55:14 +0530

    add new modules

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
+import { app } from "./app.js";
 import connectDb from "./db/index.js";
 import dotenv from "dotenv";
 dotenv.config({path:"./env"})
 
 
+connectDb()
+.then(()=>{
+    app.listen(process.env.PORT||800,()=>{
+        console.log("server is started");
 
-connectDb();
+        app.on("error",(err)=>{
+            console.log(err);
+        })
+    })
+})
+.catch((err)=>console.log(err));
