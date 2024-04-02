576a92f8426427e39e0e67264d87db1993bddbee
akshat <akshatbhatya@gmail.com>
2024-04-02 21:58:16 +0530

    video model is done

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,7 +1,45 @@
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 import mongoose from "mongoose";
 
-const videoSchema=new mongoose.Schema({},{timestamps:true})
+const videoSchema=new mongoose.Schema({
+
+    title:{
+        type:String,
+        required:true
+    },
+    discription:{
+        type:String,
+        required:true
+    },
+    thumbnail:{
+        type:String,
+        required:true
+
+    },
+    views:{
+        type:Number,
+        default:0
+    },
+    duration:{
+        type:Number,
+        required:true
+
+    },
+    owner:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"user"
+    },
+    isPublished:{
+        type:Boolean,
+        default:true
+
+    }
+    ,
+    videoFile:{
+        type:String,
+        required:true
+    }
+},{timestamps:true})
 
 videoSchema.plugin(mongooseAggregatePaginate)
 
